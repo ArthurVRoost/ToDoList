13c5637a6a60be8f461137a75a3f198c18df3e7c
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-04-25 13:34:18 +0200

    let js

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+let input = document.getElementsByClassName("input")[0];
+let btnAdd = document.getElementsByClassName("btn")[0];
+let bouton = document.getElementsByClassName("bouton")[0];
+let bouton1 = document.getElementsByClassName("bouton")[1]; 
+let bouton2 = document.getElementsByClassName("bouton")[2];
+
+let button = document.getElementsByClassName("bouton2")[0];
+let button1 = document.getElementsByClassName("bouton2")[1];
+let button2 = document.getElementsByClassName("bouton2")[2];
