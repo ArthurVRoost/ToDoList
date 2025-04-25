466b1018e4a3954b0863db902319042f9647218c
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-04-25 16:26:19 +0200

    commit vendredi 17h

--- public/js/main.js
+++ public/js/main.js
@@ -7,9 +7,10 @@ let bouton2 = document.getElementsByClassName("bouton")[2];
 let button = document.getElementsByClassName("bouton2")[0];
 let button1 = document.getElementsByClassName("bouton2")[1];
 let button2 = document.getElementsByClassName("bouton2")[2];
+let text = document.getElementById("displayText");
+let edit = false;
 
-
-function ajouter(e) {
+function ajouter(e){
     e.preventDefault()
     let liste = document.createElement("p")
     text.appendChild(liste)
@@ -20,5 +21,47 @@ function ajouter(e) {
     tache1.style.padding = "0 !important"
  }
  btnAdd.addEventListener("click", ajouter)
+  bouton.addEventListener("click", ()=>{
+    button.style.display = "none"
+    button1.style.display = "none"
+    button2.style.display = "none"
+    bouton.addEventListener("click", ()=>{
+        button.style.display = "flex"
+        button1.style.display = "flex"
+        button2.style.display = "flex"
+    })
+ })
+ button1.addEventListener("click", ()=>{
+    button.style.display = "none"
+    button2.style.display = "none"
+    let confirm = document.createElement("input")
+    confirm.textContent = "confirmer"
+    button1.insertAdjacentElement("afterend", confirm)
 
+    
+ })
+ bouton1.addEventListener("click",()=>{
+    let yesfini = document.getElementsByClassName("fini")
+    let notfini = document.getElementsByClassName("pasfini")
+    for (let i = 0; i < yesfini.length; i++) {
+       const element = yesfini[i];
+       element.style= "display:flex"
  
+    }
+    for (let i = 0; i < notfini.length; i++) {
+       const element = notfini[i];
+       element.style= "display:none"
+    }
+ })
+ bouton2.addEventListener("click",()=>{
+    let yesfini = document.getElementsByClassName("fini")
+    let notfini = document.getElementsByClassName("pasfini")
+    for (let i = 0; i < yesfini.length; i++) {
+       const element = yesfini[i];
+       element.style= "display:none"
+    }
+    for (let i = 0; i < notfini.length; i++) {
+       const element = notfini[i];
+       element.style= "display:flex"
+    }
+ })
