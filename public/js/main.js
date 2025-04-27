8ea97f77689ca322acc26bca0ea8008f6a369783
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-04-27 12:09:28 +0200

    change html

--- public/js/main.js
+++ public/js/main.js
@@ -4,64 +4,24 @@ let bouton = document.getElementsByClassName("bouton")[0];
 let bouton1 = document.getElementsByClassName("bouton")[1]; 
 let bouton2 = document.getElementsByClassName("bouton")[2];
 
-let button = document.getElementsByClassName("bouton2")[0];
-let button1 = document.getElementsByClassName("bouton2")[1];
-let button2 = document.getElementsByClassName("bouton2")[2];
 let text = document.getElementById("displayText");
 let edit = false;
 
 function ajouter(e){
     e.preventDefault()
-    let liste = document.createElement("p")
-    text.appendChild(liste)
-    let tache1= document.createElement("p")
-    tache1.textContent = String(input.value)
-    liste.appendChild(tache1)
-    tache1.style.margin = "0 !important"
-    tache1.style.padding = "0 !important"
- }
- btnAdd.addEventListener("click", ajouter)
-  bouton.addEventListener("click", ()=>{
-    button.style.display = "none"
-    button1.style.display = "none"
-    button2.style.display = "none"
-    bouton.addEventListener("click", ()=>{
-        button.style.display = "flex"
-        button1.style.display = "flex"
-        button2.style.display = "flex"
-    })
- })
- button1.addEventListener("click", ()=>{
-    button.style.display = "none"
-    button2.style.display = "none"
-    let confirm = document.createElement("input")
-    confirm.textContent = "confirmer"
-    button1.insertAdjacentElement("afterend", confirm)
-
+    if (input.value === "") 
+      return;
+    let taskContainer = document.createElement("div");
+    taskContainer.className = "task-container pasfini"
+    taskContainer.style.display = "flex"
+    taskContainer.style.alignItems = "center"
+    taskContainer.style.marginBottom = "10px"
+    taskContainer.style.width = "100%"
     
- })
- bouton1.addEventListener("click",()=>{
-    let yesfini = document.getElementsByClassName("fini")
-    let notfini = document.getElementsByClassName("pasfini")
-    for (let i = 0; i < yesfini.length; i++) {
-       const element = yesfini[i];
-       element.style= "display:flex"
- 
-    }
-    for (let i = 0; i < notfini.length; i++) {
-       const element = notfini[i];
-       element.style= "display:none"
-    }
- })
- bouton2.addEventListener("click",()=>{
-    let yesfini = document.getElementsByClassName("fini")
-    let notfini = document.getElementsByClassName("pasfini")
-    for (let i = 0; i < yesfini.length; i++) {
-       const element = yesfini[i];
-       element.style= "display:none"
-    }
-    for (let i = 0; i < notfini.length; i++) {
-       const element = notfini[i];
-       element.style= "display:flex"
-    }
- })
+    let tache1 = document.createElement("p");
+    tache1.textContent = input.value
+    tache1.style.margin = "0"
+    tache1.style.padding = "0"
+    tache1.style.marginRight = "10px"
+    tache1.style.flex = "1"
+   }
