284ffa02f9dcfad0ba73ec757691a9c715f2d6de
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-04-27 12:15:34 +0200

    ajout 3 boutons

--- public/js/main.js
+++ public/js/main.js
@@ -24,4 +24,28 @@ function ajouter(e){
     tache1.style.padding = "0"
     tache1.style.marginRight = "10px"
     tache1.style.flex = "1"
+
+    let btnFini = document.createElement("button");
+    btnFini.textContent = "Done"
+    btnFini.className = "task-btn btn-fini"
+    btnFini.style.marginRight = "5px"
+    btnFini.style.padding = "5px 10px"
+    btnFini.style.borderRadius = "5px"
+    btnFini.style.cursor = "pointer"
+
+    let btnEdit = document.createElement("button");
+    btnEdit.textContent = "Modifier"
+    btnEdit.className = "task-btn btn-edit"
+    btnEdit.style.marginRight = "5px"
+    btnEdit.style.padding = "5px 10px"
+    btnEdit.style.borderRadius = "5px"
+    btnEdit.style.cursor = "pointer"
+
+
+    let btnSupp = document.createElement("button");
+    btnSupp.textContent = "Supprimer"
+    btnSupp.className = "task-btn btn-supp"
+    btnSupp.style.padding = "5px 10px"
+    btnSupp.style.borderRadius = "5px"
+    btnSupp.style.cursor = "pointer"
    }
